f3c6da7d2a5c011d534981640c45085d4564874b
rumman321 <cset-210652@dti.ac>
2024-12-08 02:10:24 +0600

    send form data to db

--- src/Pages/Review.jsx
+++ src/Pages/Review.jsx
@@ -1,86 +1,167 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
+
 
 const Review = () => {
-    return (
-        <div className="min-h-screen flex justify-center items-center">
-          
-      <div className="card bg-base-100 w-full max-w-lg p-10 shrink-0 shadow-2xl">
-        <h2 className="font-bold text-2xl text-center">Register your account</h2>
-        <form onSubmit={handleSubmit} className="card-body">
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Your Name</span>
-            </label>
-            <input
-            name="name"
-              type="text"
-              placeholder="Name"
-              className="input input-bordered"
-              required
-            />
-          </div>
-          {/* {error.name && <label className="label text-red-600">{error.name}</label>} */}
-          {/* {
-            error.name && (
-            <label className="label">
-              {error.name}
-            </label>
-            )
-          } */}
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Photo URL</span>
-            </label>
-            <input
-              name="photo"
-              type="text"
-              placeholder="Photo url"
-              className="input input-bordered"
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input
-              name="email"
-              type="email"
-              placeholder="email"
-              className="input input-bordered"
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Password</span>
-            </label>
-            <input
-              name="password"
-              type="password"
-              placeholder="password"
-              className="input input-bordered"
-              required
-            />
+  const genres = ["comedy", "drama", "horror", "action", "thriller", "romance"];
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const form=e.target
+    const poster=form.poster.value
+    const title=form.title.value
+    const genre=form.genre.value
+    const summary=form.summary.value
+    const name=form.name.value
+    const photo=form.photo.value
 
-            {/* {error.pass && <label className="label text-red-600 text-sm">{error.pass}</label>} */}
-           
-            
-          </div>
-          <div className="form-control mt-6">
-            <button className="btn btn-primary">Register</button>
-          </div>
-        </form>
-        <br />
+    const review={ poster, title, genre, summary, name,photo }
+    console.log( poster, title, genre,  summary, name,photo)
+    const validateForm = () => {
+    
         
-        <p className="text-center font-semibold">
-          Already have an account ?{" "}
-          <Link className="text-red-600" to="/auth/login">
-            Login
-          </Link>
-        </p>
-      </div>
+      if (!poster ) {
+        return "Please provide a valid image link for the movie poster.";
+      }
+      if (!title || title.length < 2) {
+        return "Movie title must be at least 2 characters long.";
+      }
+      if (!genre) {
+        return "Please select a genre.";
+      }
+      if (!name)  {
+        return "Please provide your name.";
+      }
+     
+      
+      if (!summary || summary.length < 50) {
+        return "Comment must be at least 50 characters long.";
+      }
+      if (!photo) {
+        return "photo must be provided.";
+      }
+      return null;
+    };
+
+    const validationError = validateForm();
+    if (validationError) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: validationError,
+      });
+    } else {
+      // send data to the server
+    fetch('http://localhost:5000/review',{
+      method:'POST',
+      headers:{
+          "content-type":"application/json"
+      },
+      body:JSON.stringify(review)
+
+  })
+  .then(res=>res.json())
+  .then(data=>{
+      console.log(data)
+      Swal.fire({ 
+         icon: "success",
+         title: "Movie Added!", 
+         text: "Your review has been added successfully.", });
+  })
+      
+      
+    }
+
+  }
+    return (
+      <div className="container mx-auto p-4 pt-20">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="poster">
+            Movie Poster (URL):
+          </label>
+          <input
+            type="text"
+            name="poster"
+            // value={formData.poster}
+            // onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="title">
+            Movie Title:
+          </label>
+          <input
+            type="text"
+            name="title"
+            // value={formData.title}
+            // onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="genre">
+            Genre:
+          </label>
+          <select
+            name="genre"
+            // value={formData.genre}
+            // onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          >
+            <option value="">Select Genre</option>
+            {genres.map((genre) => (
+              <option key={genre} value={genre}>
+                {genre}
+              </option>
+            ))}
+          </select>
+        </div>
+       
+       
+        
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="summary">
+            Add Comments:
+          </label>
+          <textarea
+            name="summary"
+            // value={formData.summary}
+            // onChange={handleChange}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          ></textarea>
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+            Your Name:
+          </label>
+          <input
+            type="text"
+            name="name"
+           
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="photo">
+            Your Photo:
+          </label>
+          <input
+            type="text"
+            name="photo"
+            
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Add Movie
+          </button>
+        </div>
+      </form>
     </div>
     );
 };
